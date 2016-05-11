b7a3293db0f3f427fe2b93a205334d70e5acc8bf
royee17 <royeeg17@gmail.com>
2016-05-11 19:17:05 +0300

    Api connection - see getAttributes for instructions

---
+++ APi/Attributes.php
@@ -0,0 +1,18 @@
+<?php
+
+class Attributes {
+    
+    var $Gender; //female/male;
+    var $EyeColor; // RGB hex color value
+    var $HairColor; // RGB hex color value
+    var $HasBeard; //yes/no
+    var $HasGlasses; //yes/no
+    var $HasSmile; //yes/no
+    var $Age;
+    var $UpdateDate; //date of api check
+
+
+}
+
+
+?>
---
+++ APi/getAttributes.php
@@ -0,0 +1,21 @@
+<?php
+include("api.php");
+
+$api = new betaFaceApi();
+
+
+/*Add here the image url*/
+$face = $api->get_Image_attributes("http://www.math.tau.ac.il/~milo/design/images/tova11.jpg");
+
+/*Example- how to print the Attributes*/
+echo $api->image_Attributes->UpdateDate;
+echo "<br>";
+echo $api->image_Attributes->EyeColor;
+echo "<br>";
+echo $api->image_Attributes->HasGlasses;
+echo "<br>";
+echo $api->image_Attributes->Age;
+echo "<br>";
+echo $api->image_Attributes->Gender;
+
+?>

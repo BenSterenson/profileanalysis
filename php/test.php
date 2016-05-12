50c8491e049530439828bf936ba76ecf00ae0e0e
Ben Sterenson <bensterenson@gmail.com>
2016-05-12 19:20:37 +0300

    added comments

--- php/test.php
+++ php/test.php
@@ -1,9 +1,16 @@
 <?php
 include "dbWrapper.php";
 
+
+//## test 1 ###
+//retrieve user from sql and update first name last name by sending num:
+// 0 - dont update retrieve from database
+// 1 - update scrape facebook
 $row = 'SELECT * FROM `users` WHERE `FacebookId` = 502424842';
 $dbWrapper = new DbWrapper();
 $result = $dbWrapper->execute($row);
-$foo = new Facebook_user($result,0);
+$foo = new Facebook_user($result, 0);
 echo $foo;
+//## end test 1 ###
+
 ?> 

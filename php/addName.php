517605bd29eb2e99bf6ca05286ef096271c7d7af
Ben Sterenson <bensterenson@gmail.com>
2016-05-14 11:09:38 +0300

    adds name and number of likes

--- php/addName.php
+++ php/addName.php
@@ -1,26 +1,49 @@
 <?php
 include 'DbWrapper.php';
-#include 'Facebook_user.php';
-include 'Facebook_photo.php';
 
-
-function extract_likes_num($FacebookPhotoId){
-		$profile_pic_url =  "http://www.facebook.com/".$FacebookPhotoId;
-		
-		if (strcmp($profile_pic_url,'http://www.facebook.com/') == 0){
-			echo "no data - insert null";
-			return 0;
-		}
-		
-		$html = get_html($profile_pic_url);
-		$like_tag = '"likecount":';
-		return extract_tag($like_tag ,$html);
+function extract_html($FacebookPhotoId) {
+	$profile_pic_url =  "http://en-gb.facebook.com/".$FacebookPhotoId;
+	$html = get_html($profile_pic_url);
+	return $html;
 }
+function extract_name($html) {
+	$data = "";
+	if(preg_match('#<title id="pageTitle">(.+?)</title>#i',$html, $matches)) {
+		$data = $matches[1];
+	
+		if(strcmp($data,'Page not found | Facebook') == 0){
+			return "";
+		}
+		$data = explode(" - ", $data, 2);
+		$data = $data[1];
+		$data = preg_replace('/ added.*/', '', $data);
+		$data = preg_replace('/ updated.*/', '', $data);
+		$data = preg_replace('/ \|.*/', '', $data);
+	}
+
+	echo "<br> ## name found: $data <br>";
+	return $data;
+}
+
+function extract_likes_num($html){
+	$like_tag = '"likecount":';
+	$likes = extract_tag($like_tag ,$html);
+
+	if($likes == "" || $likes == " ")
+		$likes = 0;
+
+	return $likes;
+}
+
 
 $file = fopen("../fb_users/imported_data_cvs/users.txt", "r") or exit("Unable to open file!");
 while(!feof($file)) {
 	$line = fgets($file);
 	$uid = rtrim($line, " \t\r\n");
+	$dbWrapper = new DbWrapper();
+
+
+	/*
 	$user = new Facebook_user($uid);
 
 	if (($user->getFirstName() == 'CanvasToBlobBundle"') || ( $user->getFirstName() == 'FileHashWorkerBundle"')) {
@@ -29,29 +52,41 @@ while(!feof($file)) {
 	}
 	
 	echo "<br>".$user."<br>";
-	
-	$dbWrapper = new DbWrapper();
-	$dbWrapper->update($user);
-
-	$IdQuery = 'SELECT `Id`, `FacebookPhotoId` FROM `photos` WHERE `FacebookId` = '. $user->getUserID();
+	//$dbWrapper->update($user);
+	*/
+	$IdQuery = 'SELECT `Id`, `FacebookPhotoId` FROM `photos` WHERE `FacebookId` = '. $uid;
 	echo $IdQuery;
 	$result = $dbWrapper->execute($IdQuery);
 	$row = ($result->fetch_assoc());
 	$myId = $row['Id'];
 	$myPhotoId = $row['FacebookPhotoId'];
 
-	echo "my id: ".$myId;
-	echo "my photoid : ".$myPhotoId . "<br>";
-	$likes = extract_likes_num($myPhotoId);
-	echo "likes". $likes . "<br>";
+	if(($myPhotoId == 0) || ($myPhotoId == 10150004552801856) || ($myPhotoId == 10150004552801901) || ($myPhotoId == 10150004552801937))
+		continue;
+
+	echo "<br> my id: ".$myId;
+	echo "<br> my photoid : ".$myPhotoId;
+	$html = extract_html($myPhotoId);
+	$full_name = extract_name($html);
+	$likes = extract_likes_num($html);
+
+	list($first_name, $last_name)  = array_pad(explode(" ", $full_name, 2),2 ,null);
+		
+	if (($first_name == 'CanvasToBlobBundle"') || ( $first_name == 'FileHashWorkerBundle"')){
+		echo "<br>";
+		continue;
+	}
+
+	$user = new Facebook_user($uid,$first_name,$last_name);
+	echo "$user";
+	echo "<br> likes : $likes <br><br>";
+
+	$dbWrapper->update($user);
+
 	$LikesQuery = 'UPDATE `photos` SET `NumOfLikes`= '. $likes. ' WHERE `Id`= '.$myId;
 	echo $LikesQuery . "<br>";
 	$dbWrapper->execute($LikesQuery);
 
 
 }
-
-
-
-
 ?> 

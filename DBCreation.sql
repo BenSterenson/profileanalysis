caab09f7406842cc911eaae3d665f1ca66cf527a
liorshk <lshkiller@gmail.com>
2016-05-18 22:21:13 +0300

    Update DBCreation.sql

--- DBCreation.sql
+++ DBCreation.sql
@@ -1,52 +1,199 @@
--- Table: PhotoAttributes
-CREATE TABLE PhotoAttributes (
-    Id bigint NOT NULL AUTO_INCREMENT,
-    PhotoId bigint NOT NULL,
-    Gender bool NOT NULL,
-    EyeColor varchar(6) NOT NULL,
-    HairColor varchar(6) NOT NULL,
-    HasBeard bool NOT NULL,
-    HasGlasses bool NOT NULL,
-    HasSmile bool NOT NULL,
-    Age int NOT NULL,
-    UpdateDate date NOT NULL,
-    UpdatedByUser bool NOT NULL, 
-    CONSTRAINT PhotoAttributes_pk PRIMARY KEY (Id)
-);
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
 
--- Table: NoProfilePic
-CREATE TABLE NoProfilePic (
-    FakePhotoId bigint NOT NULL,
-    CONSTRAINT NoProfilePic_pk PRIMARY KEY (FakePhotoId)
-);
+CREATE TABLE NoProfilePic(
+	FakePhotoId bigint NOT NULL);
 
--- Table: Photos
-CREATE TABLE Photos (
-    Id  bigint NOT NULL AUTO_INCREMENT,
-    FacebookPhotoId bigint NOT NULL,
-    FacebookId bigint NOT NULL,
-    UpdateDate date NOT NULL,
-    PhotoLink varchar(255) NOT NULL,
-    NumOfLikes int NOT NULL,
-    IsValidPhoto bool NOT NULL,
-    CONSTRAINT Photos_pk PRIMARY KEY (Id)
-);
+ALTER TABLE NoProfilePic ADD PRIMARY KEY (FakePhotoId);
 
--- Table: Users
-CREATE TABLE Users (
-    FacebookId bigint NOT NULL,
-    FirstName varchar(20) NOT NULL,
-    LastName varchar(20) NOT NULL,
-    CONSTRAINT Users_pk PRIMARY KEY (FacebookId)
-);
+CREATE TABLE PhotoAttributes(
+	Id bigint NOT NULL AUTO_INCREMENT UNIQUE, 
+	PhotoId bigint NOT NULL, 
+	Gender bool NOT NULL, 
+	EyeColor varchar (6) NOT NULL, 
+	HairColor varchar (6) NOT NULL, 
+	HasBeard bool NOT NULL, 
+	HasGlasses bool NOT NULL, 
+	HasSmile bool NOT NULL, 
+	Age int NOT NULL, 
+	UpdateDate datetime NOT NULL, 
+	UpdatedByUser bool NOT NULL);
 
--- foreign keys
--- Reference: Photos_To_Attributes (table: PhotoAttributes)
-ALTER TABLE PhotoAttributes ADD CONSTRAINT Photos_To_Attributes FOREIGN KEY Photos_To_Attributes (PhotoId)
-    REFERENCES Photos (Id);
+ALTER TABLE PhotoAttributes ADD PRIMARY KEY (Id);
 
--- Reference: Users_To_Photos (table: Photos)
-ALTER TABLE Photos ADD CONSTRAINT Users_To_Photos FOREIGN KEY Users_To_Photos (FacebookId)
-    REFERENCES Users (FacebookId);
+CREATE TABLE Photos(
+	Id bigint NOT NULL AUTO_INCREMENT UNIQUE, 
+	FacebookPhotoId bigint NOT NULL, 
+	FacebookId bigint NOT NULL, 
+	UpdateDate datetime NOT NULL, 
+	PhotoLink varchar (255) NOT NULL, 
+	NumOfLikes int NOT NULL, 
+	IsValidPhoto bool NOT NULL);
 
--- End of file.
+ALTER TABLE Photos ADD PRIMARY KEY (Id);
+
+CREATE TABLE Users(
+	FacebookId bigint NOT NULL, 
+	FirstName varchar (20) NOT NULL, 
+	LastName varchar (20) NOT NULL);
+
+ALTER TABLE Users ADD PRIMARY KEY (FacebookId);
+
+CREATE TABLE History(
+	Id bigint NOT NULL AUTO_INCREMENT UNIQUE, 
+	FacebookId bigint NOT NULL, 
+	AttributeName longtext NOT NULL, 
+	FilterValue longtext NOT NULL, 
+	SessionId longtext NOT NULL);
+
+ALTER TABLE History ADD PRIMARY KEY (Id);
+
+
+CREATE TABLE PhotoComments(
+	Id int NOT NULL AUTO_INCREMENT UNIQUE, 
+	Comment longtext NOT NULL, 
+	PhotoId bigint NOT NULL, 
+	FacebookId bigint NOT NULL, 
+	Time datetime NOT NULL);
+
+ALTER TABLE PhotoComments ADD PRIMARY KEY (Id);
+
+
+CREATE TABLE PhotoRatings(
+	Id int NOT NULL AUTO_INCREMENT UNIQUE, 
+	IsHot bool NOT NULL, 
+	PhotosId bigint NOT NULL, 
+	FacebookId bigint NOT NULL);
+
+ALTER TABLE PhotoRatings ADD PRIMARY KEY (Id);
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- Creating foreign key on PhotoId in table 'PhotoAttributes'
+
+ALTER TABLE PhotoAttributes
+ADD CONSTRAINT FK_Photos_To_Attributes
+    FOREIGN KEY (PhotoId)
+    REFERENCES Photos
+        (Id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Photos_To_Attributes'
+
+CREATE INDEX IX_FK_Photos_To_Attributes
+    ON PhotoAttributes
+    (PhotoId);
+
+
+
+-- Creating foreign key on FacebookId in table 'Photos'
+
+ALTER TABLE Photos
+ADD CONSTRAINT FK_Users_To_Photos
+    FOREIGN KEY (FacebookId)
+    REFERENCES Users
+        (FacebookId)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Users_To_Photos'
+
+CREATE INDEX IX_FK_Users_To_Photos
+    ON Photos
+    (FacebookId);
+
+
+
+-- Creating foreign key on FacebookId in table 'History'
+
+ALTER TABLE History
+ADD CONSTRAINT FK_UsersHistory
+    FOREIGN KEY (FacebookId)
+    REFERENCES Users
+        (FacebookId)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UsersHistory'
+
+CREATE INDEX IX_FK_UsersHistory
+    ON History
+    (FacebookId);
+
+
+
+-- Creating foreign key on PhotoId in table 'PhotoComments'
+
+ALTER TABLE PhotoComments
+ADD CONSTRAINT FK_Photos_To_PhotoComment
+    FOREIGN KEY (PhotoId)
+    REFERENCES Photos
+        (Id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Photos_To_PhotoComment'
+
+CREATE INDEX IX_FK_Photos_To_PhotoComment
+    ON PhotoComments
+    (PhotoId);
+
+
+
+-- Creating foreign key on FacebookId in table 'PhotoComments'
+
+ALTER TABLE PhotoComments
+ADD CONSTRAINT FK_Users_To_PhotoComment
+    FOREIGN KEY (FacebookId)
+    REFERENCES Users
+        (FacebookId)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Users_To_PhotoComment'
+
+CREATE INDEX IX_FK_Users_To_PhotoComment
+    ON PhotoComments
+    (FacebookId);
+
+
+
+-- Creating foreign key on PhotosId in table 'PhotoRatings'
+
+ALTER TABLE PhotoRatings
+ADD CONSTRAINT FK_Photos_To_PhotoRating
+    FOREIGN KEY (PhotosId)
+    REFERENCES Photos
+        (Id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Photos_To_PhotoRating'
+
+CREATE INDEX IX_FK_Photos_To_PhotoRating
+    ON PhotoRatings
+    (PhotosId);
+
+
+
+-- Creating foreign key on FacebookId in table 'PhotoRatings'
+
+ALTER TABLE PhotoRatings
+ADD CONSTRAINT FK_Users_To_PhotoRating
+    FOREIGN KEY (FacebookId)
+    REFERENCES Users
+        (FacebookId)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Users_To_PhotoRating'
+
+CREATE INDEX IX_FK_Users_To_PhotoRating
+    ON PhotoRatings
+    (FacebookId);
+
